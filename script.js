c3c67698433786da7aa16ac9dafe6ab4e92626f3
Evandro <evandrocomputacao2017@gmail.com>
2020-08-10 11:26:23 -0300

    c0 - Menu de Navegacao

---
+++ script.js
@@ -0,0 +1,7 @@
+function menuToggle() {
+    let menuArea = document.getElementById("menu-area");
+
+    if(menuArea.style.width == '200px') {
+        menuArea.style.width = '0px';
+    } else {menuArea.style.width = '200px';}
+}  
